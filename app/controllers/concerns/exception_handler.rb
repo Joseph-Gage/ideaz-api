11151b7942f4e1d156363e0d290ba9c8ee9d7189
Joseph Gage <josephmillergage@gmail.com>
2018-06-24 15:24:49 +0200

    added exception handler with auth and token error handling

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,23 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
+  included do
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissingToken, with: :unprocessable_request
+    rescue_from ExceptionHandler::InvalidToken, with: :unprocessable_request
+  end
+
+  private
+
+  def unauthorized_request(exception)
+    json_response({ message: exception.message }, :unauthorized)
+  end
+
+  def unprocessable_request(exception)
+    json_response({ message: exception.message }, :unprocessable_entity)
+  end
+end
