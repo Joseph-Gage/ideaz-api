2bc9fe0da4fed7098aa46ead4e619f224bbc9db6
Joseph Gage <josephmillergage@gmail.com>
2019-04-22 19:17:09 +0200

    added presence and inclusion validations for votes and ideasa

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,5 @@
 class Idea < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :title
+  validates_presence_of :title, :user
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :idea
+
+  validates_presence_of :value, :user, :idea
+  validates_inclusion_of :value, in: [-1, 1], message: 'can only be -1 or 1'
 end
